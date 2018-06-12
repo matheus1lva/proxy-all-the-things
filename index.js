602dccb74d9412646fe84708672000402b0dcf27
PlayMa256 <matheus.g.silva@hotmail.com>
2018-06-12 14:57:51 -0300

    feat(proxy): Add functionalities for the event handler
    Added real functionality to the proxy on receive a response, also adds
    debug statements to be able to track the activity (onError and onEnd)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('proxy-all-the-things::');
+const debug = require('debug')('proxy-all-the-things::proxy');
 const request = require('request');
 
 const proxyableHeaders = ['accept', 'content-type', 'range', 'user-agent', 'cookie', 'host'];
@@ -13,7 +13,10 @@ function proxyHandler(req, res, options) {
 
 	const headers = {}
 
-	// copy all suitable headers from the inbound request to the gsedw request
+	/**
+	 * Copy all suitable headers from the inbound request 
+	 * to be sent to the destination
+	 */
 	Object.keys(req.headers)
 	.filter(header => proxyableHeaders.indexOf(header) > -1)
 	.forEach((header) => {
@@ -27,4 +30,35 @@ function proxyHandler(req, res, options) {
 		body: req.body,
 		followRedirect: false
 	});
+
+	requestStream.on('response', (response) => {
+		debug(`cid: ${correlationId} - proxied successfully. Status code: ${response.statusCode}`)
+		const headersCopy = Object.assign({}, response.headers);
+
+		// we reached a redirect
+		if (response.statusCode === 302) {
+			headersCopy.location = response.headers.location.replace(options.baseUrl, req.headers.host)
+		}
+
+		response.headers = {};
+
+		// copy all suitable headers from the gsdw response to the pending user agent response.
+		Object.keys(headersCopy).filter(h => nonReturnableHeaders.indexOf(h) === -1)
+			.forEach((h) => {
+				response.headers[h] = headersCopy[h];
+			});
+
+		requestStream.pipe(res);
+
+	}).on('end', () => {
+		debug(`cid: ${correlationId} - proxy destination answered, took ${Date.now() - start}ms`)
+	}).on('error', (error) => {
+		debug(`cid: ${correlationId} - error proxying to ${url}: ${error}`);
+	});
+}
+
+module.exports = (options) => {
+	return (req, res) => {
+		return proxyHandler(req, res, options);
+	}
 }
