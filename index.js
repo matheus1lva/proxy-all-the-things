07c9dba7a09230050c5ef983f62c4287694c87a4
PlayMa256 <matheus.g.silva@hotmail.com>
2018-06-13 14:44:17 -0300

    chore(docs): add eslint and transform middleware into a class to take advantage of single time instantiation

--- index.js
+++ index.js
@@ -4,61 +4,70 @@ const request = require('request');
 const proxyableHeaders = ['accept', 'content-type', 'range', 'user-agent', 'cookie', 'host'];
 const nonReturnableHeaders = ['server', 'host', 'content-length', 'x-powered-by', 'date', 'connection', 'x-powered-by'];
 
-function proxyHandler(req, res, options) {
-	const correlationId = uuid();
-	const start = Date.now();
-	const url = `${options.baseUrl}${req.url.replace(options.internalPath, '')}`;
-	
-	debug(`${correlationId} proxying to: ${url}`);
+/** 
+ * @typedef {Object} ProxyConfig 
+ * @property {String} baseUrl
+ * @property {String} internalPath
+ */
 
-	const headers = {}
-
+module.exports = class Proxy{
 	/**
-	 * Copy all suitable headers from the inbound request 
-	 * to be sent to the destination
+	 * @param {ProxyConfig} config 
 	 */
-	Object.keys(req.headers)
-	.filter(header => proxyableHeaders.indexOf(header) > -1)
-	.forEach((header) => {
-		headers[header] = req.headers[header];
-	});
-	
-	const requestStream = request({
-		method: req.method,
-		url,
-		headers,
-		body: req.body,
-		followRedirect: false
-	});
+	constructor(config){
+		this.config = config;
+	}
 
-	requestStream.on('response', (response) => {
-		debug(`cid: ${correlationId} - proxied successfully. Status code: ${response.statusCode}`)
-		const headersCopy = Object.assign({}, response.headers);
+	proxyHandler(req, res) {
+		const correlationId = uuid();
+		const start = Date.now();
+		const url = `${this.config.baseUrl}${req.url.replace(this.config.internalPath, '')}`;
+		
+		debug(`${correlationId} proxying to: ${url}`);
 
-		// we reached a redirect
-		if (response.statusCode === 302) {
-			headersCopy.location = response.headers.location.replace(options.baseUrl, req.headers.host)
-		}
+		const headers = {}
 
-		response.headers = {};
+		/**
+		 * Copy all suitable headers from the inbound request 
+		 * to be sent to the destination
+		 */
+		Object.keys(req.headers)
+		.filter(header => proxyableHeaders.indexOf(header) > -1)
+		.forEach((header) => {
+			headers[header] = req.headers[header];
+		});
+		
+		const requestStream = request({
+			method: req.method,
+			url,
+			headers,
+			body: req.body,
+			followRedirect: false
+		});
 
-		// copy all suitable headers from the gsdw response to the pending user agent response.
-		Object.keys(headersCopy).filter(h => nonReturnableHeaders.indexOf(h) === -1)
-			.forEach((h) => {
-				response.headers[h] = headersCopy[h];
-			});
+		requestStream.on('response', (response) => {
+			debug(`cid: ${correlationId} - proxied successfully. Status code: ${response.statusCode}`)
+			const headersCopy = Object.assign({}, response.headers);
 
-		requestStream.pipe(res);
+			// we reached a redirect
+			if (response.statusCode === 302) {
+				headersCopy.location = response.headers.location.replace(this.config.baseUrl, req.headers.host)
+			}
 
-	}).on('end', () => {
-		debug(`cid: ${correlationId} - proxy destination answered, took ${Date.now() - start}ms`)
-	}).on('error', (error) => {
-		debug(`cid: ${correlationId} - error proxying to ${url}: ${error}`);
-	});
-}
+			response.headers = {};
 
-module.exports = (options) => {
-	return (req, res) => {
-		return proxyHandler(req, res, options);
+			// copy all suitable headers from the gsdw response to the pending user agent response.
+			Object.keys(headersCopy).filter(h => nonReturnableHeaders.indexOf(h) === -1)
+				.forEach((h) => {
+					response.headers[h] = headersCopy[h];
+				});
+
+			requestStream.pipe(res);
+
+		}).on('end', () => {
+			debug(`cid: ${correlationId} - proxy destination answered, took ${Date.now() - start}ms`)
+		}).on('error', (error) => {
+			debug(`cid: ${correlationId} - error proxying to ${url}: ${error}`);
+		});
 	}
 }
